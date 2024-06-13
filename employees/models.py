0edfbe27664047d590d2c351fca3fb5baf5a9882
Harish <bsharish152@gmail.com>
2024-06-13 18:21:59 +0530

    REST_FRAMEWORK

--- employees/models.py
+++ employees/models.py
@@ -3,9 +3,13 @@ from django.db import models
 class employee(models.Model):
     first_name = models.CharField(max_length=100)
     last_name = models.CharField(max_length=100)
+    #photo = models.ImageField(upload_to='images')
     designation = models.CharField(max_length=100)
     email_address = models.EmailField(max_length=100, unique=True)
     phone_number = models.CharField(max_length=12, blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     # 11/06/24
+
+    def __str__(self):
+        return f'{self.first_name} {self.last_name}'

3bfd8ac610a8ddcee0870dac364e58c0a867df38
Harish <bsharish152@gmail.com>
2024-06-13 18:22:25 +0530

    REST_FRAMEWORK upt 1

--- employees/views.py
+++ employees/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import employee
+from .serializers import employeesSerializers
+from django.http import JsonResponse
 
-# Create your views here.
+def employee_list(request):
+    Employees = employee.objects.all()
+    serializer = employeesSerializers(Employees, many = True)
+    return JsonResponse(serializer.data, safe = False)
--- mysite/urls.py
+++ mysite/urls.py
@@ -21,7 +21,11 @@ from . import views
 from django.conf import settings
 from django.conf.urls.static import static
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
-] + static(settings.MEDIA_URL,documents_root=settings.MEDIA_ROOT)
+    #path('api-auth/', include('rest_framework.urls'))
+    path('employee/', views.employee_list),
+    path('employee/<int:id>', views.employee_detail),
+ ] + static(settings.MEDIA_URL,documents_root=settings.MEDIA_ROOT)
